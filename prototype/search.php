23d37aadfee36b7b2be344b0fd6312d68b7d341d
edunton <edunton@bu.edu>
2014-10-31 17:37:31 -0400

    Changed around some things
    referenced css correctly in prototype and renamed css folder to styles

--- prototype/search.php
+++ prototype/search.php
@@ -24,7 +24,7 @@ if(count($_POST) > 0) {
 <html>
     <head>
         <title>Nutrition Search Prototype</title>
-        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="../styles/bootstrap.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
