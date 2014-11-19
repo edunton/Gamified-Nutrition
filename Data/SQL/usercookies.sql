ec5b263ad9a7250d3e6d5f0be1a64fa173ca2511
edunton <edunton@bu.edu>
2014-11-18 19:48:49 -0500

    Updated tables and working user facade
    The user facade will work with these tables to produce the user profiles
    and set there cookies

--- Data/SQL/usercookies.sql
+++ Data/SQL/usercookies.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2014 at 01:43 AM
+-- Generation Time: Nov 19, 2014 at 01:45 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,24 +23,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userprofiles`
+-- Table structure for table `usercookies`
 --
 
-CREATE TABLE IF NOT EXISTS `userprofiles` (
-  `userID` varchar(40) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL DEFAULT '',
-  `username` varchar(40) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL,
-  `passwordHash` varchar(300) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL
+CREATE TABLE IF NOT EXISTS `usercookies` (
+  `cookie` varchar(300) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL,
+  `userID` varchar(40) CHARACTER SET utf32 COLLATE utf32_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `userprofiles`
---
-ALTER TABLE `userprofiles`
- ADD PRIMARY KEY (`userID`), ADD UNIQUE KEY `userID` (`userID`), ADD UNIQUE KEY `username` (`username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
---
+++ Interfaces/StaticFixture.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eric
+ * Date: 11/12/2014
+ * Time: 8:53 AM
+ */
+
+namespace Interfaces;
+
+
+interface StaticFixture {
+    public static function display();
+} 
