c0779b89378901a0bad73f31a010a03ef24d07a2
whitecatgsd <gaosida@bu.edu>
2014-12-06 15:20:00 -0500

    Create profileedit.php

---
+++ profileedit.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sida Gao
+ * Date: 12/6/2014
+ * Time: 1:28 PM
+ */
+
+require_once 'Main.php';
+
+$hp = new \Display\ProfileEdit();
+echo $hp->getPage();
+
+?>
