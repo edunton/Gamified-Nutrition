e11e49ee0a3f3bb4938bdee0a76e296a3d09892a
whitecatgsd <gaosida@bu.edu>
2014-12-06 15:41:07 -0500

    Update HomePage.php

--- Display/HomePage.php
+++ Display/HomePage.php
@@ -113,7 +113,7 @@ EOD;
                 echo '</table></div>';
             } else {
 
-                echo '<div class="alert alert-info" role="alert">No history so far!</div>';
+                echo '<div class="text-center alert alert-info" role="alert">No history so far!</div>';
             }
         };
 
